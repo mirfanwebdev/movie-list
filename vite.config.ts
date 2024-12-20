5c941c47423dea5f783901169221278eadcddec4
m.irfan <m.irfanwebdev@gmail.com>
2024-12-20 17:29:13 +0700

    initialize vite, vue, tailwind, shadcn-ui

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'url'
+import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+
+// https://vite.dev/config/
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        tailwind(),
+        autoprefixer(),
+      ]
+    }
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
